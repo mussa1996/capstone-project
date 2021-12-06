d3184fb70f50d9b3fd59ea8b89317600dcdc7363
Niyodusenga Mussa <niyodusengamussa01@gmail.com>
2021-12-06 09:01:45 +0200

    adding responsiveness

--- UI/javascript/script.js
+++ UI/javascript/script.js
@@ -1,11 +1,37 @@
-$(document).ready(()=>{
-    $(window).scroll(()=>{
-        if(this.scrollY>20){
-            $('.navbar').addClass("upDown");
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
         }
-        else
-        {
-            $('.navbar').removeClass("upDown");
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
         }
-    })
-})
+    });
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+    // smooth scroll script
+    $('.navbar .menu li a').click(function(){
+        $('html').animate({scrollTop: $($(this).attr('href')).offset().top - 50});
+    }
+    );
+});
