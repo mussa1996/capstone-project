8ffe5eaede132de51009dfb012765202a98eaebe
Niyodusenga Mussa <niyodusengamussa01@gmail.com>
2021-12-08 17:40:27 +0200

    adding js on menu

---
+++ UI/javascript/menuBar.js
@@ -0,0 +1,15 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
