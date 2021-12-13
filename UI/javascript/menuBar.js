b199be68ec9261ddfe9fce475826790354c9df8f
Niyodusenga Mussa <niyodusengamussa01@gmail.com>
2021-12-13 14:44:16 +0200

    modified menu js

--- UI/javascript/menuBar.js
+++ UI/javascript/menuBar.js
@@ -1,15 +1,10 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
-  function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
+const menu = document.querySelector(".menu-bar");
+const nav= document.querySelector(".nav-link");
+menu.addEventListener("click", function(){
+    nav.classList.toggle("active");
+}
+);
+nav.addEventListener("click", function(){
+    nav.classList.toggle("active");
+}
+);
