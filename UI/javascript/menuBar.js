d775f6ead3d2a0e9b66667f386853d32e1d8d146
Niyodusenga Mussa <niyodusengamussa01@gmail.com>
2021-12-13 11:47:17 +0200

    adding menu for responsiveness

--- UI/javascript/menuBar.js
+++ UI/javascript/menuBar.js
@@ -1,10 +1,13 @@
 
 
-const userList = document.querySelector(".menu-btn");
-const nav= document.querySelector(".max-width");
-console.log(userList);
-userList.addEventListener('click', userList);
-function test(){
-  console.log('hey');
+const menu = document.querySelector(".menu-bar");
+const nav= document.querySelector(".nav-link");
+menu.addEventListener("click", function(){
+    nav.classList.toggle("active");
 }
+);
+nav.addEventListener("click", function(){
+    nav.classList.toggle("active");
+}
+);
 
