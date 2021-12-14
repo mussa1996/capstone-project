b2aad4145307e0106249d0257bbcc8a9d42a9561
Niyodusenga Mussa <niyodusengamussa01@gmail.com>
2021-12-14 11:14:24 +0200

    modified validate

--- UI/javascript/validation-form.js
+++ UI/javascript/validation-form.js
@@ -4,11 +4,36 @@ const names = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const form = document.getElementById('form');
-const errorElement=document.getElementById('error');
+const errorUsername=document.getElementById('username-error');
+const errorEmail=document.getElementById('email-error');
+const errorPassword=document.getElementById('password-error');
 const errorlogin=document.getElementById('error-login');
 const form1=document.getElementById('form1');
 form.addEventListener('submit',(e)=>{
     let messages=[];
+    // e.preventDefault();
+    // if(names.value===''||names.value==null){
+    //     errorUsername.innerHTML="please enter your name"
+    //     setTimeout(function(){
+    //         errorUsername.fadeOut();
+    //     },3000);
+    // //    if(names.length>0)
+    // //      {  e.preventDefault();
+    // //         errorUsername.remove(); 
+    // //         }
+            
+    // }
+    // if(email.value===''||email.value==null){
+       
+    //         errorEmail.innerHTML="Please enter your email"
+    //     }
+    //      if(password.value===''||password.value==null){
+           
+    //     errorPassword.innerHTML="Please enter your password"
+    // }
+    // if(messages.length>0){
+    //     errorUsername.innerHTML="please enter your name"
+    // }
     if(names.value===''||names.value==null){
         messages.push('Name is required');
         
@@ -18,10 +43,13 @@ form.addEventListener('submit',(e)=>{
     }
     if(password.value===''||password.value==null){
         messages.push('Password is required');
+        if(password.length<8){
+            messages.push('password must be at least 8 characters');
+        }
     }
     if(messages.length>0){
         e.preventDefault();
-        errorElement.innerText=messages.join(',');
+        errorUsername.innerText=messages.join(',');
     }
 })
 
