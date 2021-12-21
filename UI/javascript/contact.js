a48c5af9773161ef73d22d75309769a25a2e9d47
Niyodusenga Mussa <niyodusengamussa01@gmail.com>
2021-12-21 22:31:53 +0200

    modified

--- UI/javascript/contact.js
+++ UI/javascript/contact.js
@@ -1,9 +1,14 @@
 
+// let form = document.querySelector('#myContactForm')
+
+
 let form = document.querySelector('#myContactForm')
 
 form.addEventListener('submit', (e)=>{
         e.preventDefault();
-       
+       const name=document.getElementById("name").value;
+        const email=document.getElementById("email").value;
+        const message=document.getElementById("message").value;
           db.collection('messages').add({
             usename: form.username.value,
             email: form.email.value,
@@ -14,3 +19,18 @@ form.addEventListener('submit', (e)=>{
         })
         
         })
+
+        const firebaseConfig = {
+          apiKey: "AIzaSyBZRUa2SA5tgZKtfE9wi2ribWrX76nEu60",
+          authDomain: "capstone-project-7f578.firebaseapp.com",
+          projectId: "capstone-project-7f578",
+          storageBucket: "capstone-project-7f578.appspot.com",
+          messagingSenderId: "913886745605",
+          appId: "1:913886745605:web:a7ac1dc1fc74f72e27b86c"
+        }; 
+        // Initialize Firebase
+        firebase.initializeApp(firebaseConfig);
+        firebase.analytics();
+        const db = firebase.firestore();
+        console.log("initialize firebase")
+        db.settings({timestampsInSnapshot:true});
